f7da4d003ecc30b364c755e3dc69161ca960910b
unknown <vitorito260476@gmail.com>
2025-07-22 22:41:08 -0300

    Integrando Alllure Reports e Configurando Newman

--- tests/newman-runner.js
+++ tests/newman-runner.js
@@ -1,17 +1,18 @@
-// Testando o Newman para executar uma coleção do Postman;
+const { exec } = require('child_process');
 
-const newman = require('newman');
-
-newman.run({
-  collection: require('../postman/collection.json'),
-  environment: require('../postman/environment.json'),
-  reporters: ['cli', 'html'],
-  reporter: {
-    html: {
-      export: './newman/report.html',
-    }
+exec('allure generate ./allure-results --clean -o ./allure-report', (error, stdout, stderr) => {
+  if (error) {
+    console.error('Erro ao gerar o relatório Allure:', error);
+    console.error('stderr:', stderr);
+    return;
   }
-}, (err) => {
-  if (err) throw err;
-  console.log('Collection executada com sucesso!');
+  console.log('Relatório Allure gerado com sucesso!');
+  exec('allure open ./allure-report', (error2, stdout2, stderr2) => {
+    if (error2) {
+      console.error('Erro ao abrir o relatório Allure:', error2);
+      console.error('stderr:', stderr2);
+      return;
+    }
+    console.log('Relatório Allure aberto no navegador!');
+  });
 });
